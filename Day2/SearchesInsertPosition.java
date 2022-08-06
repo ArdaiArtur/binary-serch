b95ace8a3d617aaadb6e9c143c4645c985428fbc
Artur Ardai <krkerdo@gmail.com>
2022-08-06 15:06:48 +0300

    ye

---
+++ Day2/SearchesInsertPosition.java
@@ -0,0 +1,24 @@
+package Day2;
+
+public class SearchesInsertPosition {
+    
+    public int searchInsert(int[] nums, int target) {
+        int left=0;int right =nums.length-1;
+             int mid=0;
+             while(left<=right)
+             {
+                  mid=(left+right+1)/2;
+                 if(nums[mid]==target) return mid;
+                 if(nums[mid]>target)
+                 {
+                     right=mid-1;
+                 }
+                 else
+                 {
+                     left=mid+1;
+                 }
+             }
+             return left;
+         }
+
+}
