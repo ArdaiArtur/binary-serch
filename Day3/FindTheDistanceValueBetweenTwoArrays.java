dfc922965b4ce2d482dda45e7410fef09cfe380a
Artur Ardai <krkerdo@gmail.com>
2022-08-07 13:37:26 +0300

    ye

--- Day3/FindTheDistanceValueBetweenTwoArrays.java
+++ Day3/FindTheDistanceValueBetweenTwoArrays.java
@@ -8,24 +8,20 @@ public class FindTheDistanceValueBetweenTwoArrays {
     }
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
         Arrays.sort(arr2);
-        int count=arr1.length;
-        for (int i = 0; i < arr2.length; i++) {
-            
-            int str=0,db=0;
-            int end=arr2.length-1;
-            while (str<=end) {
-                int mid=str+(end-str)/2;
-                if(d>=Math.abs(arr1[i]-arr2[mid]))
-                {
-                     count--;
-                     break;
+        int count = arr1.length;
+        for(int i: arr1){
+            int start = 0, end = arr2.length-1, mid;
+            while(start <= end){
+                mid = start + (end-start)/2;
+                if(d >= Math.abs(-i + arr2[mid])){
+                    count--;
+                    break;
                 }
-                else
-                {
-                    if(arr2[mid]>i)
-                    end=mid-1;
+                else{
+                    if(arr2[mid] > i)
+                        end = mid - 1;
                     else
-                    str=mid+1;
+                        start = mid + 1;
                 }
             }
         }
