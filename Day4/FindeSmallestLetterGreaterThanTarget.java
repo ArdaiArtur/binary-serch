abd9f9c9572e61f6e7a5508dddfdd4f1d17d9b88
Artur Ardai <krkerdo@gmail.com>
2022-08-08 15:16:51 +0300

    ye

--- Day4/FindeSmallestLetterGreaterThanTarget.java
+++ Day4/FindeSmallestLetterGreaterThanTarget.java
@@ -3,19 +3,24 @@ package Day4;
 public class FindeSmallestLetterGreaterThanTarget {
     
     public char nextGreatestLetter(char[] letters, char target) {
-        int str=0;int set=0;
-        int end=letters.length-1;
         if(target==letters[letters.length-1]) return letters[0];       
-        while(str<=end)
+        int l=0;
+        int r=letters.length;
+        while(l<r)
         {
-            int mid=str+(end-str)/2;
-            if(letters[mid]==target) {set=mid;break;}
-            if(letters[mid]>target) end=mid-1;
+            int mid=l+(r-l)/2;       
+            if(letters[mid]>target)
+            {
+                r=mid;
+            }
             else
-            str=mid+1;
+            {l=mid+1;}
+
         }
-        if(set!=0)return letters[set+1];
-        return letters[str%letters.length];
         
+        return letters[l%letters.length];
+    
+            
+    
     }
 }
--- Day4/test.java
+++ Day4/test.java
@@ -2,7 +2,7 @@ package Day4;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.PriorityQueue;
+
 
 public class test {
     
