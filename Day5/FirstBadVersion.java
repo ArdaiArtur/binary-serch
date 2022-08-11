9be55e06d2653a8852ee63804febdc550038f742
Artur Ardai <krkerdo@gmail.com>
2022-08-11 16:42:22 +0300

    ye

---
+++ Day5/FirstBadVersion.java
@@ -0,0 +1,16 @@
+package Day5;
+
+public class FirstBadVersion {
+    public int firstBadVersion(int n) {
+        int start=0, end=n;
+       while(start<end){
+           int mid = start + (end-start)/2;
+           if (isBadVersion(mid)){
+               end=mid;
+           } else {
+               start=mid+1;
+           }
+       }
+       return start;
+   }
+}
