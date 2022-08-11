831ee04e4596b2a22a0dfaeada49c9eea61cba44
Artur Ardai <krkerdo@gmail.com>
2022-08-11 18:24:44 +0300

    s

--- Day6/KthMissingNumber.java
+++ Day6/KthMissingNumber.java
@@ -1,11 +1,26 @@
 package Day6;
 
 public class KthMissingNumber {
-    public int findKthPositive(int[] arr, int k) {
+    public int findKthPositivem(int[] arr, int k) {
         int count=arr[0]-1;
         for (int i = 1; i < arr.length; i++) {
             if(arr[i-1]+1!=arr[i]) count++;
         }
         return count;
+    }
+
+    public int findKthPositive(int[] arr, int k) {
+        if(arr[0] > k) {
+            return k;
+        }
+        
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] <= k) {
+                k++;
+            }else {
+                break;
+            }
+        }
+        return k;
     }
 }
